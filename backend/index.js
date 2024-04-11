2c84555506434e6928a094ddb8e388deb9d54deb
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-04-11 18:01:07 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const multer = require("multer");
 const app = express();
 const fs = require("fs");
 app.use(cors());
-app.use(function (req, res, next) {
+/*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
   res.header(
@@ -13,7 +13,7 @@ app.use(function (req, res, next) {
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});
+});*/
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static("uploads"));
