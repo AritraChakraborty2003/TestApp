db285f754a5120a0e11d8394c1d54691dd17cbfc
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-04-11 18:15:12 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -9,6 +9,14 @@ app.use(
     origin: "https://urtechguru.online",
   })
 );
+app.use(function (request, response, next) {
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 /*app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
