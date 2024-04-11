238419eb63e8b0a1c8789f2dd4065a255a64d296
AritraChakraborty2003 <aritra.chakraborty203@gmail.com>
2024-04-11 17:12:21 +0530

    new commit

--- backend/index.js
+++ backend/index.js
@@ -7,8 +7,7 @@ const fs = require("fs");
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(__dirname + "/public"));
-app.use("/uploads", express.static("uploads"));
+app.use(express.static("uploads"));
 
 var storage = multer.diskStorage({
   destination: function (req, file, cb) {
